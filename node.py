cd7b7072f9c9981509938c32f8eda5e7a59be12b
Puneethkrishna-2003 <111770848+Puneethkrishna-2003@users.noreply.github.com>
2022-12-09 20:18:19 +0530

    Update node.py

--- node.py
+++ node.py
@@ -6,10 +6,7 @@ class Node:
 
    def insert(self, data):
 # Compare the new value with the parent node
-      if self.data:
-         if data < self.data:
-            if self.left is None:
-               self.left = Node(data)
+    
             else:
                self.left.insert(data)
             elif data > self.data:
